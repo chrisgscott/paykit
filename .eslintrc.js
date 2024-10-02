9b4a039130a037cd2eeae50d8867d469788455c3
chrisgscott <chrisgscott@gmail.com>
2024-10-02 12:36:13 -0600

    Set up supabase, stripe, sentry, eslint, prettier, added secrets to env

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    parser: '@typescript-eslint/parser',
+    extends: [
+      'next/core-web-vitals',
+      'plugin:@typescript-eslint/recommended',
+      'plugin:react/recommended',
+      'plugin:react-hooks/recommended',
+      'prettier'
+    ],
+    rules: {
+      // Add any custom rules here
+    }
+  };
